f12abf4243d65fd643874f86e56f8cbeea531f93
raifbrah <raifbrah@gmail.com>
2023-01-30 16:15:53 +0300

    test

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === "production" ? "/vue-todo/" : "/",
-// };
-
 module.exports = {
-  publicPath: "/vue-todo/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-todo/" : "/",
 };
